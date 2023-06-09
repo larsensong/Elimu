2d5ef5af3f103092a03dd48daec32255e6d28501
root <larsenmulamba@gmail.com>
2023-06-09 20:28:45 +0000

    update

---
+++ models/task.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" task class """
+
+from models import Base
+import models
+from models.core import Core
+from sqlalchemy import Column, Integer, String, ForeignKey, Text
+
+
+class Task(Core, Base):
+    """ class for task """
+    __tablename__ = 'task'
+    pid = Column(String(60), ForeignKey('project.id'))
+    name = Column(String(128))
+    description = Column(Text)
+    weight = Column(Integer)
+    avg_denom = Column(Integer)
+    output = Column(String(128))
+
+    @classmethod
+    def get_all_tasks(cls, pid):
+        lst = models.db.all('Task')
+        all = []
+        for obj in lst:
+            if obj.pid == pid:
+                all.append(obj)
+        return all
